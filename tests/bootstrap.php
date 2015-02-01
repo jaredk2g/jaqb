93f57f5373e6bcdfc02afba48a7829350938d449
Jared King <j@jaredtking.com>
2015-02-01 09:46:55 -0600

    remove extra command in test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,6 +12,4 @@ error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', true);
 date_default_timezone_set('America/Chicago');
 
-exec('rm -rf '.dirname(__DIR__).'/temp');
-
 require __DIR__."/../vendor/autoload.php";
