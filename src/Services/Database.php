7dae2bce46ed4a6945de2d2138170e67cb3f16fa
Jared King <j@jaredtking.com>
2016-01-05 11:22:50 -0600

    added database service for infuse framework

---
+++ src/Services/Database.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @author Jared King <j@jaredtking.com>
+ *
+ * @link http://jaredtking.com
+ *
+ * @copyright 2015 Jared King
+ * @license MIT
+ */
+namespace JAQB\Services;
+
+use JAQB\QueryBuilder;
+
+class Database
+{
+    public function __invoke($app)
+    {
+        return new QueryBuilder($app['pdo']);
+    }
+}
